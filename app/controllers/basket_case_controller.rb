46fd2c3942602afb8d79a763f33f1aa9339c5a8c
xiema <maxprincipe@yahoo.com>
2023-11-09 12:20:32 +0800

    implement asc/desc order functions

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,16 +1,21 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
   before_action :set_items
+  before_action :set_order
 
   def index; end
 
   def order_by_fruit
     @list_items = @list_items.sort
+    @list_items = @list_items.reverse if @order == "desc"
+    @order_type = :fruit
     render 'basket_case/index'
   end
 
   def order_by_amount
     @list_items = @list_items.sort_by(&:last)
+    @list_items = @list_items.reverse if @order == "desc"
+    @order_type = :amount
     render 'basket_case/index'
   end
 
@@ -26,5 +31,14 @@ class BasketCaseController < ApplicationController
 
     def set_items
       @list_items = @fruits
+    end
+
+    def set_order
+      @order = case params[:order]
+      when "desc"
+        "desc"
+      else
+        "asc" 
+      end
     end
 end
--- app/helpers/basket_case_helper.rb
+++ app/helpers/basket_case_helper.rb
@@ -1,2 +1,21 @@
 module BasketCaseHelper
+    def get_inverse_order
+        if @order == "desc" then "asc" else "desc" end
+    end
+
+    def get_default_order
+        "asc"
+    end
+
+    def get_default_order_name
+        "Ascending"
+    end
+
+    def get_order_name
+        if @order == "desc" then "Descending" else "Ascending" end
+    end
+
+    def get_inverse_order_name
+        if @order == "desc" then "Ascending" else "Descending" end
+    end
 end
