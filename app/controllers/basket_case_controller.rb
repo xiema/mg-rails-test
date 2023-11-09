0499b821e2f4dbae2d738d6476d77132afd7fb38
xiema <maxprincipe@yahoo.com>
2023-11-09 12:34:26 +0800

    implement sort by amount with asc/desc

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,18 +1,21 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
   before_action :set_items
+  before_action :set_order
 
   def index; end
 
   def order_by_fruit
     @list_items = @list_items.sort
-    @list_items = @list_items.reverse
+    @list_items = @list_items.reverse if @order == "desc"
+    @order_type = :fruit
     render 'basket_case/index'
   end
 
   def order_by_amount
-    # code here
-
+    @list_items = @list_items.sort_by(&:last)
+    @list_items = @list_items.reverse if @order == "desc"
+    @order_type = :amount
     render 'basket_case/index'
   end
 
@@ -29,5 +32,14 @@ class BasketCaseController < ApplicationController
 
     def set_items
       @list_items = @fruits
+    end
+
+    def set_order
+      @order = case params[:order]
+      when "desc"
+        "desc"
+      else
+        "asc"
+      end
     end
 end
--- app/helpers/basket_case_helper.rb
+++ app/helpers/basket_case_helper.rb
@@ -1,2 +1,18 @@
 module BasketCaseHelper
+  def get_default_order
+    "asc"
+  end
+
+  def get_order_name(order)
+    case order
+    when "asc"
+        "Ascending"
+    when "desc"
+        "Descending"
+    end
+  end
+
+  def get_inverse_order
+    if @order == "asc" then "desc" else "asc" end
+  end
 end
